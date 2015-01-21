2a39fde81253e352b5967d5184926721762f198c
iliastsangaris <iliastsangaris@gmail.com>
2015-01-20 16:27:22 -0800

    books show up

--- app/assets/javascripts/books.js
+++ app/assets/javascripts/books.js
@@ -3,14 +3,28 @@ var Library = {}
 
 // Fetch all books
 Library.loadBooks = function() { // Fetch all the books
-  console.log("Where are all the books")
+  $.get("/books").done(function(books){
+    Library.appendBooks(books); //Appends the books
+  });
 }
 
+
+Library.appendBooks = function(books) {
+  var ul = $("<ul>").attr("id", "book-ul");
+  //Iterate through all the books
+  books.forEach(function(book){
+    var li = $("<li>").text(book.title + " by " + book.author);
+    var deleteBtn = $("<button>").text("X").attr("onclick", "Library.deleteBook(event)");
+    li.append(deleteBtn);
+    ul.append(li);
+  });
+  $("#container").append(ul);
+}
 
 // Add a book
 Library.addBook = function() {
 }
 
 // Delete a book
-Library.deleteBook = function() {
+Library.deleteBook = function(event) {
 }
