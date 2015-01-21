004b9cf7feb5498b496e464d91ab1114bbd01ee0
iliastsangaris <iliastsangaris@gmail.com>
2015-01-20 16:34:17 -0800

    deleting

--- app/assets/javascripts/books.js
+++ app/assets/javascripts/books.js
@@ -13,7 +13,7 @@ Library.appendBooks = function(books) {
   var ul = $("<ul>").attr("id", "book-ul");
   //Iterate through all the books
   books.forEach(function(book){
-    var li = $("<li>").text(book.title + " by " + book.author);
+    var li = $("<li>").text(book.title + " by " + book.author).attr("id", book.id);
     var deleteBtn = $("<button>").text("X").attr("onclick", "Library.deleteBook(event)");
     li.append(deleteBtn);
     ul.append(li);
@@ -27,4 +27,12 @@ Library.addBook = function() {
 
 // Delete a book
 Library.deleteBook = function(event) {
+  var targetId = $(event.target).parent().attr("id");
+  $.ajax({
+    url: "/books/" + targetId,
+    method: "DELETE"
+  }).done(function(){
+    $("li#" + targetId).remove();
+    console.log("deleted!");
+  });
 }
